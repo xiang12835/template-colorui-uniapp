4b794ab3a622b8337d146563bf8947098b84470c
yu.xiang <yu.xiang@alibaba-inc.com>
2020-05-05 12:56:27 +0800

    init code

---
+++ pages/index/index.vue
@@ -0,0 +1,30 @@
+<template>
+	<view class="content">
+		{{title}}
+	</view>
+</template>
+<script>
+	import { test200 } from "@/apis/test.js"
+	export default {
+	    data() {
+	        return {
+	            title: ''
+	        }
+	    },
+	    onLoad() {
+	        this.test()
+	    },
+	    methods: {
+	        // 测试获取数据
+	        test () {
+	            test200().then(res => {
+	                this.title = res.data
+	            })
+	        }
+	    }
+	}
+</script>
+
+<style>
+
+</style>
